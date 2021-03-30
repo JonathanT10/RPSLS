589946824a87e51ba7ba4d7e475af18a10dd63af
Jonathan Terrell <jonathan.terrell@stewartorg.com>
2021-03-29 21:17:36 -0500

    game.js Added playOption method to pick single or multiplayer. players.js Added selectGesture method to choose gesture.

--- players.js
+++ players.js
@@ -1,19 +1,52 @@
-class Players {
+let prompt = require("prompt-sync")();
+
+class Player {
     constructor(name){
         this.score = 0;
         this.name = name;
         this.gesture = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+        this.choosenGesture = this.selectGesture;
+    }
+
+    selectGesture(){
+        let pick = prompt("Pick your move: \n1. " + this.gesture[0] + " \n2. " + this.gesture[1] + " \n3. " + this.gesture[2] + " \n4. " + this.gesture[3] + " \n5. " + this.gesture["4"] + "\n");
+        switch(pick){
+            case "1":
+                console.log(this.gesture[0])
+            break;
+            case "2":
+                console.log(this.gesture[1])
+            break;
+            case "3":
+                console.log(this.gesture[2])
+            break;
+            case "4":
+                console.log(this.gesture[3])
+            break;
+            case "5":
+                console.log(this.gesture[4])
+            break;
+        }
+        return pick;
     }
 }
 
-class Human extends Players {
+class Human extends Player {
     constructor(name){
         super(name);
     }
 }
 
-class AI extends Players {
+class AI extends Player {
     constructor(name){
         super(name);
     }
 }
+let game = new Player();
+game.playOption();
+game.selectGesture();
+
+
+module.exports.Player = Player;
+module.exports.Human = Human;
+module.exports.AI = AI;
